9571710fc80341639f6955bcfd2fd03c7f190f73
sooraj <soorajnambiar8@gmail.com>
2024-05-11 19:07:05 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Preloader from "./components/preloader/Preloader";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true); // Initially show the preloader
 
-export default App
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false); // Hide the preloader after 3 seconds
+    }, 4000);
+
+    return () => clearTimeout(timeoutId); // Cleanup function to prevent memory leaks
+  }, []);
+
+  return (
+    <>
+      {isLoading && <Preloader />}  {/* Conditionally render the preloader */}
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default App;
