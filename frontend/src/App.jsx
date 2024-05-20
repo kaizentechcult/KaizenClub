c82327853ad9f087ebcc8b8d120bfc42d0b270b8
Sooraj <soorajnambiar8@gmail.com>
2024-05-20 18:25:52 +0530

    added style to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div
-        class="b1"
-        className="h-80 w-80 bg-blue-400 rounded-full blur-[200px] mx-120"
-      ></div>
-      <div
-        class="b2"
-        className="h-80 w-80 bg-blue-600 rounded-full blur-[150px] "
-      ></div>
+      <h1>hello</h1>
     </>
   );
 };
--- frontend/src/components/Navbar/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar/Navbar.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="logo"></div>
-      <ul className="flex gap-20 m-5 absolute right-2 text-2xl">
-        <li>Home</li>
-        <li>Members</li>
-        <li>Projects</li>
-        <li>Join Us</li>
+    <div className="navbar flex">
+      <div className="logo">
+        <img
+          src="https://i.pinimg.com/originals/1c/54/f7/1c54f7b06d7723c21afc5035bf88a5ef.png"
+          alt="logo"
+          className="h-10 rounded-full m-4"
+        />
+      </div>
+      <ul className="flex gap-20 m-5 absolute right-2 text-2xl text-[#5d5dff] ">
+        <li><a href="#" className="hover:text-white">Home</a></li>
+        <li><a href="#" className="hover:text-white">Members</a></li>
+        <li><a href="#" className="hover:text-white">Projects</a></li>
+        <li><a href="#" className="hover:text-white">Join Us</a></li>
       </ul>
-    </>
+    </div>
   );
 };
 
