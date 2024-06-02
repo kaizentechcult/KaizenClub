27cb9ab22605d08310d19ac4922ac1de909d37a3
Sooraj <soorajnambiar8@gmail.com>
2024-06-02 23:58:25 +0530

    ui changes login

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <>
+    {/* <div className="bg-[#242d5e] rounded-full p-4"> */}
+      <img src="Logo2.svg" className="w-16 h-16" />
+      <p className="text-white text-2xl font-bold">Kaizen Society</p>
+    {/* </div> */}
+    </>
+  );
+};
+
+export default Logo;
