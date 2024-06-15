432db6861bb9ea9d66215923af9604663cb65b71
Sooraj <soorajnambiar8@gmail.com>
2024-06-15 23:57:07 +0530

    Updates

--- assets/LazyImg-D75tu6n4.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-BVoJ8giS.js";const o=()=>a.jsx("img",{className:"lg:h-[85vh] rounded-3xl scale-75 object-cover",src:"hero.jpg",alt:"img",loading:"lazy"});export{o as default};
