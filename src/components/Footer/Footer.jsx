a0cbacfffd214bf5b355840d4b76d720d8612172
Sooraj <soorajnambiar8@gmail.com>
2024-06-19 19:41:50 +0530

    changed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-// import XIcon from "@mui/icons-material/X";
 import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Footer = () => {
   return (
-    <div className="w-full flex flex-col bg-[#ceecff] py-12 rounded-t-xl items-center justify-center text-black gap-10">
+    <div className="w-full flex flex-col bg-[#141941] py-12 rounded-t-xl items-center justify-center text-[#a2c9ff] gap-10">
       <div className="flex flex-col justify-center items-center">
-        <img src="LogoK.svg" alt="" className="w-8" />
+        <div className="h-32 w-32 flex justify-center items-center bg-[#dfecff] rounded-full">
+          <img src="LogoK.svg" alt="" className="w-10" />
+        </div>
         <br />
         <p className="text-xl font-bold">Kaizen Tech Club</p>
       </div>
-      <div className="w-full h-[2px] bg-black"></div>
+      <div className="w-full h-[2px] bg-white"></div>
       <div className=" flex gap-10">
         <a aria-label="Github" href="https://github.com/kaizentechcult">
           <GitHubIcon className="scale-125" />
         </a>
-        {/* <a href="https://github.com/kaizentechcult">
-          <XIcon className="scale-125" />
-        </a> */}
-        <a aria-label="LinkedIn" href="https://www.linkedin.com/company/kaizen-technical-society/">
+        <a
+          aria-label="LinkedIn"
+          href="https://www.linkedin.com/company/kaizen-technical-society/"
+        >
           <LinkedInIcon className="scale-125" />
         </a>
-        <a aria-label="Instagram" href="https://www.instagram.com/kcc_technical_society">
+        <a
+          aria-label="Instagram"
+          href="https://www.instagram.com/kcc_technical_society"
+        >
           <InstagramIcon className="scale-125" />
         </a>
       </div>
