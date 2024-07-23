faf73e811080eaecc9124adc2412e76fb688bc8e
Sooraj <soorajnambiar8@gmail.com>
2024-07-24 00:28:13 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
             element={
               <>
                 <Home />
-                <Footer />
+                {/* <Footer /> */}
               </>
             }
           />
