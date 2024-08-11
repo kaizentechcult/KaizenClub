fd45f7168e4e1b76f8ced1f50aceba8232c90da2
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-11 23:36:36 +0530

    working on it

--- src/pages/Team/components/BWD/BWD.jsx
+++ src/pages/Team/components/BWD/BWD.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-// import img1 from "/Team/BWD/img1.jpg";
-import img2 from "/Team/BWD/img2.webp";
-import img3 from "/Team/BWD/img3.webp";
-import img4 from "/Team/BWD/img4.webp";
-import img5 from "/Team/BWD/img5.webp";
-import img6 from "/Team/BWD/img6.webp";
-import img7 from "/Team/BWD/img7.webp";
-import img8 from "/Team/BWD/img8.webp";
-import img9 from "/Team/BWD/img9.webp";
+// import img1 from "/Team/BWD/img1.webp";
+// import img2 from "/Team/BWD/img2.webp";
+// import img3 from "/Team/BWD/img3.webp";
+// import img4 from "/Team/BWD/img4.webp";
+// import img5 from "/Team/BWD/img5.webp";
+// import img6 from "/Team/BWD/img6.webp";
+// import img7 from "/Team/BWD/img7.webp";
+// import img8 from "/Team/BWD/img8.webp";
+// import img9 from "/Team/BWD/img9.webp";
 
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
@@ -19,14 +19,14 @@ import Heading from "../../../../components/Heading/Heading";
 const BWD = () => {
   const teamMembers = [
     // { img: img1, alt: "Member 1" },
-    { img: img2, alt: "Member 2" },
-    { img: img3, alt: "Member 3" },
-    { img: img5, alt: "Member 5" },
-    { img: img4, alt: "Member 4" },
-    { img: img8, alt: "Member 8" },
-    { img: img6, alt: "Member 6" },
-    { img: img7, alt: "Member 7" },
-    { img: img9, alt: "Member 9" },
+    { img: "/Team/BWD/img1.webp", alt: "Member 2" },
+    { img: "/Team/BWD/img2.webp", alt: "Member 3" },
+    { img: "/Team/BWD/img3.webp", alt: "Member 5" },
+    { img: "/Team/BWD/img4.webp", alt: "Member 4" },
+    { img: "/Team/BWD/img5.webp", alt: "Member 8" },
+    { img: "/Team/BWD/img6.webp", alt: "Member 6" },
+    { img: "/Team/BWD/img7.webp", alt: "Member 7" },
+    { img: "/Team/BWD/img9.webp", alt: "Member 9" },
   ];
 
   return (
